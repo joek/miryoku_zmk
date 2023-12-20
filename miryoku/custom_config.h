254fa72f5074f6a292fba57229ba9caa2a2e29cb
Johannes Engelke <johannes.engelke@sap.com>
2023-12-20 22:11:52 +0100

    Add Backlight

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,7 +2,10 @@
 // https://github.com/manna-harbour/miryoku
 
 #include <dt-bindings/pwm/pwm.h>
+#include <dt-bindings/zmk/bt.h>
 #include <dt-bindings/zmk/backlight.h>
+
+# define CONFIG_ZMK_USB_LOGGING=y
 
 #define CONFIG_ZMK_BACKLIGHT y
 #define CONFIG_PWM y
